639bb3df3ab0f1dde9cefa618efa5a74e6f1c86d
Xinran1205 <id19037@bristol.ac.uk>
2025-03-31 12:09:02 +0100

    init

---
+++ src/sjdb/Optimiser.java
@@ -0,0 +1,12 @@
+package sjdb;
+
+public class Optimiser {
+    public Optimiser(Catalogue cat) {
+        // initialise optimiser
+    }
+
+    public Operator optimise(Operator plan) {
+        // create and return a new, optimised plan based on the plan you're passed
+    }
+    
+}
